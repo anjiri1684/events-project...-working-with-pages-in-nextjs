f21725c98fa790df23780d70d538a17d74ec08b1
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-22 12:14:45 +0300

    first commit

---
+++ pages/[...slug].js
@@ -0,0 +1,7 @@
+function FilteredEventPage() {
+  <div>
+    <h1>Filtered Events</h1>
+  </div>;
+}
+
+export default FilteredEventPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+function EventsPage() {
+  return (
+    <div>
+      <h1>All Events </h1>
+    </div>
+  );
+}
+
+export default EventsPage;
